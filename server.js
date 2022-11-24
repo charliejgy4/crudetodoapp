4405efb4b45e2bece8570409c9c948ade810f57e
Charlie Joseph <charliejgy4@gmail.com>
2022-11-24 03:10:58 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const dotenv = require('dotenv');
+const TodoTask =require('./models/TodoTask');
+dotenv.config();
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs"); 
+
+
+app.get('/',(req, res) => {
+    TodoTask.find({}, (err, tasks)=>{
+    res.render('todo.ejs', {todoTasks:tasks});
+});
+});
+
+ app.use('/static', express.static('public'));
+
+
+
+//POST METHOD
+app.post('/',async (req, res) => {
+    const todoTask = new TodoTask({
+    content: req.body.content
+    });
+    try {
+    await todoTask.save();
+    res.redirect("/");
+    } catch (err) {
+    res.redirect("/");
+    }
+    });
+
+// GET METHOD
+app.get("/", (req, res) => {
+    TodoTask.find({}, (err, tasks) => {
+    res.render("todo.ejs", { todoTasks: tasks });
+    });
+    });
+
+
+    //UPDATE
+app.route("/edit/:id")
+app.get((req, res) => {
+const id = req.params.id;
+TodoTask.find({}, (err, tasks) => {
+res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+});
+});
+
+app.post((req, res) => {
+const id = req.params.id;
+TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+if (err) return res.send(500, err);
+res.redirect("/");
+});
+});
+
+
+//DELETE
+app.route("/remove/:id").get((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndRemove(id, err => {
+    if (err) return res.send(500, err);
+    res.redirect("/");
+    });
+    });
+
+   
+    
+    mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+        console.log("Connected to database!");
+app.listen(4000, () => console.log("Server is live"));
+
+});
+
